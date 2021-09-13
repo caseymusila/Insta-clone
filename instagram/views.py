752a4233c6a237f45899ee704d850b54c9494cfe
caseymusila <musilacasey@gmail.com>
2021-09-13 05:10:12 +0300

    we first import our models which will be post and comment

--- instagram/views.py
+++ instagram/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from .models import Post, Comment
+
+def index(request):
+    post = Post.objects.all().order_by("-created")
+
+    context = {
+        "posts": post,
+    }
+
+    return render(request, "grampost/home.html", context)
 
 
 
