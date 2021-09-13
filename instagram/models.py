16f88e950fb0e7410865278b1bd3e78d1e0d9740
caseymusila <musilacasey@gmail.com>
2021-09-13 05:57:36 +0300

    we create our first model and will be post.

--- instagram/models.py
+++ instagram/models.py
@@ -4,3 +4,16 @@ from cloudinary.models import CloudinaryField
 
 
 # Create your models here.
+class Post(models.Model):
+    image = CloudinaryField('image')
+    image_name = models.CharField(max_length = 100)
+    image_caption = models.TextField(max_length = 500)
+    created = models.DateTimeField(auto_now_add = True)
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
+    likes = models.IntegerField(null = True, default = 0)
+
+    def __str__(self):
+        return self.image_name
+
+    def delete(self):
+        self.delete()
