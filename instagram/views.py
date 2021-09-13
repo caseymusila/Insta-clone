8e556fbc49a810ee4db1837fa0d6e3b2e493d89c
caseymusila <musilacasey@gmail.com>
2021-09-13 12:27:49 +0300

    in our views we first import forms to the view page

--- instagram/views.py
+++ instagram/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Post
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_protect
+from .forms import PostForm, CommentForm
 
 
 
