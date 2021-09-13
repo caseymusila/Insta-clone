008a1cbb1a49cab398fc1ab7057bc361a321a1d6
caseymusila <musilacasey@gmail.com>
2021-09-13 12:21:06 +0300

    create a form instance of postform

--- instagram/forms.py
+++ instagram/forms.py
@@ -1,5 +1,11 @@
 from instagram.models import NewPost
 from django import forms
+from cloudinary.models import CloudinaryField
 
+
+class PostForm(forms.Form):
+    image = forms.ImageField()
+    image_name = forms.CharField()
+    image_caption = forms.CharField(widget=forms.Textarea())
 
 
